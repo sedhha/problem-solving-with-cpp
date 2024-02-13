d0441c5ce41004dd201fb3c44669fbc1b84c02f6
sedhha <activity.schoolsh2@gmail.com>
2024-02-13 12:58:58 +0530

    frogJumpN -> 'tabulation form'

--- problems/dp/frogjumpN.cpp
+++ problems/dp/frogjumpN.cpp
@@ -20,6 +20,26 @@ int f(int n, vector<int> v, vector<int> &dp, int k)
     return dp[n] = energy;
 }
 
+int fTab(int n, vector<int> v, int k)
+{
+    vector<int> dp(k, 0);
+    dp[0] = 0;
+    for (int i = 1; i < n; i++)
+    {
+        int energy = INT_MAX;
+        for (int j = 1; j <= k; j++)
+        {
+            if (i - j >= 0)
+            {
+                int energyForNthJump = dp[i - j] + abs(v[i] - v[i - j]);
+                energy = min(energy, energyForNthJump);
+            }
+        }
+        dp[i] = energy;
+    }
+    return dp[n - 1];
+}
+
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -33,5 +53,6 @@ int main()
         cin >> input[i];
     vector<int> dp(n + 1, -1);
     cout << "Result: " << f(n - 1, input, dp, k) << endl;
+    cout << "Result fTab: " << fTab(n, input, k) << endl;
     return 0;
 }
